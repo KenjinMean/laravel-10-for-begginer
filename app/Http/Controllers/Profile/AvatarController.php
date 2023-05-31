3bbdff954dca4bf0938c5edce552e1f8770a67d4
Jin <genesisjinsaquibal@gmail.com>
2023-05-31 22:26:54 +0800

    code for avatar image storing

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller {
+    public function update(UpdateAvatarRequest $request) {
+        $path = $request->file('avatar')->store('avatars');
+        $path = str_replace('/', '\\', $path);
+        auth()->user()->update(['avatar' => storage_path('app\\' . "$path")]);
+        return back()->with('message', 'Avatar is updated.');
+    }
+}
