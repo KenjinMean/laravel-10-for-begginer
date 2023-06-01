ec77a0a82b78f530af031e4ba1612df9184029cb
Jin <genesisjinsaquibal@gmail.com>
2023-06-01 19:59:54 +0800

    updated file to accomodate update user cover

--- resources/views/profile/partials/user-avatar-form.blade.php
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -22,8 +22,29 @@
     <div>
       <x-input-label for="avatar" value="Avatar" />
       <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" autofocus
-        autocomplete="avatar" />
+        autocomplete="avatar" required />
       <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+    </div>
+
+    <div class="flex items-center gap-4">
+      <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+      @if (session('status') === 'profile-updated')
+        <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+          class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
+      @endif
+    </div>
+  </form>
+
+  <form method="post" action="{{ route('profile.cover') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
+    @csrf
+    @method('patch')
+
+    <div>
+      <x-input-label for="cover" value="Cover" />
+      <x-text-input id="cover" name="cover" type="file" class="mt-1 block w-full" :value="old('cover', $user->cover)" autofocus
+        autocomplete="cover" required/>
+      <x-input-error class="mt-2" :messages="$errors->get('cover')" />
     </div>
 
     <div class="flex items-center gap-4">
