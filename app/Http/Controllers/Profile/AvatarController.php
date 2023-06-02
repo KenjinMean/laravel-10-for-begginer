6f24632b58f5f39fd377e998a79c94b74cb64176
Jin <genesisjinsaquibal@gmail.com>
2023-06-02 14:58:02 +0800

    refactored code to use storage facade, added feature where when user updates avatar, it deletes the old existing avatar

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller {
-    public function update(UpdateAvatarRequest $request) {
-        $path = $request->file('avatar')->store('avatars');
-        $path = str_replace('/', '\\', $path);
-        auth()->user()->update(['avatar' => storage_path('app\\' . "$path")]);
-        return back()->with('message', 'Avatar is updated.');
+  public function update(UpdateAvatarRequest $request) {
+    // $path = $request->file('avatar')->store('public\avatars');
+    // $path = $request->file('avatar')->store('avatars', 'public');
+    $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+    $path = str_replace('\\', '/', $path);
+
+    // remove old avatar if not equal to default avatar
+    $oldAvatar = $request->user()->avatar;
+
+    if ($oldAvatar !== "avatars/default-avatar.png") {
+      Storage::disk('public')->delete($oldAvatar);
     }
+
+    auth()->user()->update(['avatar' => $path]);
+
+    return back()->with('message', 'Avatar is updated.');
+  }
 }
