753dd0cd854085f48382249fce503e6305bd0b04
Jin <genesisjinsaquibal@gmail.com>
2023-06-02 15:03:30 +0800

    store server public path instead of local path

--- app/Http/Controllers/Profile/CoverController.php
+++ app/Http/Controllers/Profile/CoverController.php
@@ -9,12 +9,12 @@ use App\Http\Requests\UpdateCoverRequest;
 class CoverController extends Controller {
     public function update(UpdateCoverRequest $request) {
 
-        $path = $request->file('cover')->store('covers');
-        $path = str_replace('/', '\\', $path);
+        // $path = $request->file('cover')->store('public\covers');
+        $path = $request->file('cover')->store('covers', 'public');
+        $path = str_replace('\\', '/', $path);
 
         $user = Auth::user();
-
-        $user->cover = storage_path('app\\' . $path);
+        $user->cover = $path;
         $user->save();
 
         // auth()->user()->update(['cover' => storage_path('app\\' . "$path")]);
